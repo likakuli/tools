250ef43a28f613ad55fad8e626e87b9702a99b68
likakuli <1154584512@qq.com>
2022-02-27 18:48:29 +0800

    add github action icon
    Signed-off-by: likakuli <1154584512@qq.com>

---
+++ test/test_test.go
@@ -0,0 +1,14 @@
+package test
+
+import (
+	"testing"
+)
+
+func TestAdd(t *testing.T){
+	t.Run("test add", func(t *testing.T) {
+		r:=Add(1,2)
+		if r!=3{
+			t.Errorf("expetcd: %d, but got: %d",3,r)
+		}
+	})
+}
