6782eabb87d9ea128707c73ef5f7ef05dc204b89
likakuli <1154584512@qq.com>
2022-02-27 19:23:24 +0800

    add github action icon
    Signed-off-by: likakuli <1154584512@qq.com>

--- test/test.go
+++ test/test.go
@@ -1,5 +1,5 @@
 package test
 
-func Add(a,b int) int{
-	return a+b
+func Add(a, b int) int {
+	return a + b
 }
--- test/test_test.go
+++ test/test_test.go
@@ -4,11 +4,11 @@ import (
 	"testing"
 )
 
-func TestAdd(t *testing.T){
+func TestAdd(t *testing.T) {
 	t.Run("test add", func(t *testing.T) {
-		r:=Add(1,2)
-		if r!=3{
-			t.Errorf("expetcd: %d, but got: %d",3,r)
+		r := Add(1, 2)
+		if r != 3 {
+			t.Errorf("expetcd: %d, but got: %d", 3, r)
 		}
 	})
 }
